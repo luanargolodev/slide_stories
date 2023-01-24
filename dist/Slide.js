8201b76ebebc040308f6ef771a5855bd4b3bcb7b
Luan Lemos <luan.lemos@agenciaeplus.com.br>
2023-01-24 19:14:16 -0300

    feat: dar funcionalidade ao slide

--- dist/Slide.js
+++ dist/Slide.js
@@ -1,14 +1,50 @@
 export default class Slide {
     container;
-    elements;
+    slides;
     controls;
     time;
-    constructor(container, elements, controls, time = 5000) {
+    index;
+    slide;
+    constructor(container, slides, controls, time = 5000) {
         this.container = container;
-        this.elements = elements;
+        this.slides = slides;
         this.controls = controls;
         this.time = time;
-        console.log(this.container, this.elements, this.controls, this.time);
+        this.index = 0;
+        this.slide = this.slides[this.index];
+        this.show(this.index);
+        this.init();
+    }
+    hide(el) {
+        el.classList.remove("active");
+    }
+    show(index) {
+        this.index = index;
+        this.slide = this.slides[this.index];
+        this.slides.forEach((slide) => this.hide(slide));
+        this.slide.classList.add("active");
+    }
+    prev() {
+        const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+        this.show(prev);
+    }
+    next() {
+        const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+        this.show(next);
+    }
+    addControls() {
+        const prevButton = document.createElement("button");
+        const nextButton = document.createElement("button");
+        this.controls.appendChild(prevButton);
+        this.controls.appendChild(nextButton);
+        prevButton.innerText = "Slide anterior";
+        nextButton.innerText = "Próximo slide";
+        prevButton.addEventListener("pointerup", () => this.prev());
+        nextButton.addEventListener("pointerup", () => this.next());
+    }
+    init() {
+        this.addControls();
+        this.show(this.index);
     }
 }
 //# sourceMappingURL=Slide.js.map
--- src/Slide.ts
+++ src/Slide.ts
@@ -1,20 +1,65 @@
 export default class Slide {
   container
-  elements
+  slides
   controls
   time
+  index
+  slide
 
   constructor(
     container: Element,
-    elements: Element[],
+    slides: Element[],
     controls: Element,
     time: number = 5000
   ) {
     this.container = container
-    this.elements = elements
+    this.slides = slides
     this.controls = controls
     this.time = time
+    this.index = 0
+    this.slide = this.slides[this.index]
+    this.show(this.index)
 
-    console.log(this.container, this.elements, this.controls, this.time)
+    this.init()
+  }
+
+  hide(el: Element) {
+    el.classList.remove("active")
+  }
+
+  show(index: number) {
+    this.index = index
+    this.slide = this.slides[this.index]
+    this.slides.forEach((slide) => this.hide(slide))
+    this.slide.classList.add("active")
+  }
+
+  prev() {
+    const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1
+    this.show(prev)
+  }
+
+  next() {
+    const next = this.index + 1 < this.slides.length ? this.index + 1 : 0
+    this.show(next)
+  }
+
+  private addControls() {
+    const prevButton = document.createElement("button")
+    const nextButton = document.createElement("button")
+
+    this.controls.appendChild(prevButton)
+    this.controls.appendChild(nextButton)
+
+    prevButton.innerText = "Slide anterior"
+    nextButton.innerText = "Próximo slide"
+
+    prevButton.addEventListener("pointerup", () => this.prev())
+    nextButton.addEventListener("pointerup", () => this.next())
+  }
+
+  private init() {
+    this.addControls()
+    this.show(this.index)
   }
 }
